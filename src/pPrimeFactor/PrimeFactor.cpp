7a9ab09ca96d1dcd05d351f2de3351a41a46ef03
Paal Arthur S. Thorseth <paalthorseth@gmail.com>
2020-02-16 15:41:41 -0500

    Add pPrimeFactor w/ uTimerScript

---
+++ src/pPrimeFactor/PrimeFactor.cpp
@@ -0,0 +1,119 @@
+/************************************************************/
+/*    NAME: Paal Arthur S. Thorseth                                              */
+/*    ORGN: MIT                                             */
+/*    FILE: PrimeFactor.cpp                                        */
+/*    DATE:                                                 */
+/************************************************************/
+
+#include <iterator>
+#include "MBUtils.h"
+#include "PrimeFactor.h"
+
+using namespace std;
+
+//---------------------------------------------------------
+// Constructor
+
+PrimeFactor::PrimeFactor()
+{
+
+}
+
+//---------------------------------------------------------
+// Destructor
+
+PrimeFactor::~PrimeFactor()
+{
+}
+
+//---------------------------------------------------------
+// Procedure: OnNewMail
+
+bool PrimeFactor::OnNewMail(MOOSMSG_LIST &NewMail)
+{
+  MOOSMSG_LIST::iterator p;
+   
+  for(p=NewMail.begin(); p!=NewMail.end(); p++) {
+    CMOOSMsg &msg = *p;
+
+#if 0 // Keep these around just for template
+    string key   = msg.GetKey();
+    string comm  = msg.GetCommunity();
+    double dval  = msg.GetDouble();
+    string sval  = msg.GetString(); 
+    string msrc  = msg.GetSource();
+    double mtime = msg.GetTime();
+    bool   mdbl  = msg.IsDouble();
+    bool   mstr  = msg.IsString();
+#endif
+
+    string key   = msg.GetKey();
+    int dval     = msg.GetDouble();
+
+    if (key == "NUM_VALUE")
+    {
+      if (dval%2==0)
+        Notify("NUM_RESULT", to_string(dval) + ", even");
+      else
+        Notify("NUM_RESULT", to_string(dval) + ", odd");
+    }
+  }
+   return(true);
+}
+
+//---------------------------------------------------------
+// Procedure: OnConnectToServer
+
+bool PrimeFactor::OnConnectToServer()
+{
+   RegisterVariables();
+   return(true);
+}
+
+//---------------------------------------------------------
+// Procedure: Iterate()
+//            happens AppTick times per second
+
+bool PrimeFactor::Iterate()
+{
+  return(true);
+}
+
+//---------------------------------------------------------
+// Procedure: OnStartUp()
+//            happens before connection is open
+
+bool PrimeFactor::OnStartUp()
+{
+  list<string> sParams;
+  m_MissionReader.EnableVerbatimQuoting(false);
+  if(m_MissionReader.GetConfiguration(GetAppName(), sParams)) {
+    list<string>::iterator p;
+    for(p=sParams.begin(); p!=sParams.end(); p++) {
+      string line  = *p;
+      string param = tolower(biteStringX(line, '='));
+      string value = line;
+      
+      if(param == "foo") {
+        //handled
+      }
+      else if(param == "bar") {
+        //handled
+      }
+    }
+  }
+  
+  RegisterVariables();	
+  return(true);
+}
+
+//---------------------------------------------------------
+// Procedure: RegisterVariables
+
+void PrimeFactor::RegisterVariables()
+{
+  // Register("FOOBAR", 0);
+
+  Register("NUM_VALUE", 0);
+}
+
